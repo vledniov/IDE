e8afb5a283b2f8a0230a8bde54d6e88d944e49b2
Vladislav Ledniov <vledniov@nebulent.com>
2013-03-02 21:43:23 +0200

    Introducing bash compile testing

--- HelloWorldPrograms/c/hello.c
+++ HelloWorldPrograms/c/hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
- 
+
 int main()
 {
+  cout << "shit";
   printf("Hello world\n");
   return 0;
 }
--- test.sh
+++ test.sh
@@ -1,17 +1,34 @@
 #! /bin/zsh/
 echo "Hello from python!"
 python HelloWorldPrograms/python/hello.py
+if [ $? -ne 0 ]
+then
+  echo "Python compile failed"
+fi
 
 echo "Hello from Ruby!"
 ruby HelloWorldPrograms/ruby/hello.rb
+if [ $? -ne 0 ]
+then
+  echo "Ruby compile failed"
+fi
 
 echo "Hello from C!"
 gcc HelloWorldPrograms/c/hello.c -o hola
-./hola
-rm hola
+if [ $? -ne 0 ]
+then
+    echo "C compile failed"
+else
+  ./hola
+  rm hola
+fi
 
 echo "Hello from C++!"
 g++ HelloWorldPrograms/cpp/hello.cpp -o hola
-./hola
-rm hola
-
+if [ $? -ne 0 ]
+then
+  echo "C++ compile failed"
+else
+  ./hola
+  rm hola
+fi
